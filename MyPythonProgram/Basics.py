1bdda399071894f77fd901ad12e2a206ac59f345
mmamytov <>
2018-07-27 06:53:16 -0500

    Reset --soft HEAD~~ command

--- MyPythonProgram/Basics.py
+++ MyPythonProgram/Basics.py
@@ -20,17 +20,23 @@ print('one more commit needed from Branch3 because this line is added')
 print('hello guys this master branch commit')
 
 print('Helllo I made some changes on Branch 3')
-
-
-
-
-
-
-
-
-
-
 #git remote add origin https://github.com/MemoNano/Mamat.git
+print('this is Memo file and newly added')
+ 
+"""
+This is from MEMO branch
+"""
++"""
+-	+This is from MEMO branch
++"""
+
+"""
+Master branch change
+""" 
+
+
+
+
 
 
 
---
+++ MyPythonProgram/Helloworld.py
@@ -0,0 +1,5 @@
+"""
+Hello World , my name is Memo and I live in Russia since I was born.
+"""
+for i in range(0,100,5):
+    print(i)
